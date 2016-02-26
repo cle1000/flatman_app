5629ad42b2c414ee7933acdc04268bd7e4a41740
Clemens <cle1000.cb@gmail.com>
2016-02-26 13:24:22 +0100

    add platform android

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -38,8 +38,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
-        alert("init");
-        cordova.InAppBrowser.open("http://www.flatman.at", "_self", "location=no");
+        alert("init")
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
